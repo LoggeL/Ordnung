eca8f3bf4e25b68f7b1cbebd873f23aef2a94fa3
LoggeL <hyper.xjo@gmail.com>
2023-04-22 01:28:25 +0200

    Use TUI Grid

--- pb_public/main.js
+++ pb_public/main.js
@@ -1,39 +1,86 @@
-const pb = new PocketBase()
+const pb = new PocketBase('https://ordnung.logge.top')
+
+// Tui Grid
+const Grid = tui.Grid
+Grid.applyTheme('striped', {
+  selection: {
+    background: '#4daaf9',
+    border: '#004082',
+  },
+  scrollbar: {
+    background: '#f5f5f5',
+    thumb: '#d9d9d9',
+    active: '#c1c1c1',
+  },
+  row: {
+    even: {
+      background: '#f3ffe3',
+    },
+    hover: {
+      background: '#ccc',
+    },
+  },
+  cell: {
+    normal: {
+      background: '#fbfbfb',
+      border: '#e0e0e0',
+      showVerticalBorder: true,
+    },
+    header: {
+      background: '#eee',
+      border: '#ccc',
+      showVerticalBorder: true,
+    },
+    rowHeader: {
+      border: '#ccc',
+      showVerticalBorder: true,
+    },
+    editable: {
+      background: '#fbfbfb',
+    },
+    selectedHeader: {
+      background: '#d8d8d8',
+    },
+    focused: {
+      border: '#418ed4',
+    },
+    disabled: {
+      text: '#b0b0b0',
+    },
+  },
+})
+
+// Custom cell renderer
+class BtnCellRenderer {
+  constructor(props) {
+    const el = document.createElement('button')
+
+    el.classList.add('cell-btn', 'border')
+    el.disabled = props.value === ''
+    el.innerText = props.value === '' ? 'No link' : 'Open'
+    el.onclick = props.columnInfo.renderer.clicked.bind(this, props.value)
+    el.setAttribute('alt', props.value)
+
+    const div = document.createElement('div')
+    div.classList.add('tooltip')
+    div.innerText = props.value
+
+    el.appendChild(div)
+
+    this.el = el
+    this.render(props)
+  }
+
+  getElement() {
+    return this.el
+  }
+
+  render(props) {}
+}
 
 // Blacklist columns
 const blacklist = ['collectionId', 'collectionName', 'created', 'id', 'expand']
 let collections = {}
-
-class BtnCellRenderer {
-  constructor() {
-    this.eGui = null
-    this.btnClickedHandler = this.btnClickedHandler.bind(this)
-  }
-
-  init(params) {
-    this.params = params
-
-    this.eGui = document.createElement('button')
-    this.eGui.classList.add('small')
-    this.eGui.style['margin-top'] = '5px'
-    if (this.params.value === '') this.eGui.setAttribute('disabled', 'true')
-    this.eGui.innerText = this.params.value === '' ? 'No URL' : 'Open'
-
-    this.eGui.addEventListener('click', this.btnClickedHandler)
-  }
-
-  getGui() {
-    return this.eGui
-  }
-
-  btnClickedHandler(event) {
-    this.params.clicked(this.params.value)
-  }
-
-  destroy() {
-    this.eGui.removeEventListener('click', this.btnClickedHandler)
-  }
-}
 
 pb.admins
   .authWithPassword('logge@duck.com', '404noswagfound')
@@ -70,7 +117,8 @@ pb.admins
 function loadCollection(collectionName) {
   document.getElementById('grid').innerHTML = ''
 
-  document.getElementById('name').innerText = collectionName
+  document.getElementById('name').innerText =
+    collectionName.slice(0, 1).toUpperCase() + collectionName.slice(1)
 
   // Close sidebar
   document.getElementById('collections').classList.remove('active')
@@ -82,24 +130,92 @@ function loadCollection(collectionName) {
 
       // Create column definitions
 
-      console.log(collections[collectionName].schema)
       const columnDefs = Object.values(collections[collectionName].schema).map(
         (val) => {
+          if (val.name.toLowerCase() == 'ort') {
+            console.log('Ort')
+            return {
+              name: val.name,
+              header: val.name,
+              renderer: {
+                type: BtnCellRenderer,
+                clicked: async function (field) {
+                  const tags = field.split('_')
+
+                  // last tag is the id
+                  const id = tags.pop()
+
+                  // get the collection name from the remaining tags
+                  const collection = tags.join('_')
+
+                  const modal = document.getElementById('location-modal')
+
+                  pb.collection(collection)
+                    .getOne(id)
+                    .then((data) => {
+                      console.log(data)
+
+                      modal.querySelector('h5').innerText =
+                        data.name || 'No name'
+
+                      const table = modal.querySelector('table')
+                      table.innerHTML = ''
+
+                      for (const key in data) {
+                        if (
+                          key === 'name' ||
+                          blacklist.includes(key) ||
+                          typeof data[key] !== 'string'
+                        )
+                          continue
+                        // Table
+                        table.innerHTML += `
+                          <tr>
+                            <td>${key}</td>
+                            <td>${data[key]}</td>
+                          </tr>
+                        `
+                      }
+                    })
+                    .catch((err) => {
+                      console.log(err)
+                    })
+                    .finally(() => {
+                      ui('#location-modal')
+                    })
+                },
+              },
+            }
+          }
+
+          console.log(val.type)
           switch (val.type) {
             case 'text':
-              return { field: val.name, sortable: true, type: val.type }
+              return {
+                name: val.name,
+                sortable: true,
+                type: val.type,
+                header: val.name,
+              }
             case 'url':
               return {
-                field: val.name,
-                cellRenderer: BtnCellRenderer,
-                cellRendererParams: {
-                  clicked: function (field) {
-                    window.open(field, '_blank')
+                name: val.name,
+                header: val.name,
+
+                renderer: {
+                  type: BtnCellRenderer,
+                  clicked: function (link) {
+                    window.open(link, '_blank')
                   },
                 },
               }
             default:
-              return { field: val.name, sortable: true, type: val.type }
+              return {
+                field: val.name,
+                sortable: true,
+                type: val.type,
+                header: val.name,
+              }
           }
         }
       )
@@ -109,25 +225,16 @@ function loadCollection(collectionName) {
         (e) => !blacklist.includes(e.field)
       )
 
-      const rowData = data
-
-      const defaultColDef = {
-        // make every column editable
-        editable: true,
-        // make columns resizable
-        resizable: true,
-      }
-
-      // let the grid know which columns and what data to use
-      const gridOptions = {
-        columnDefs: columnDefsFiltered,
-        rowData: rowData,
-        defaultColDef: defaultColDef,
-      }
-
       // setup the grid after the page has finished loading
       const gridDiv = document.getElementById('grid')
-      new agGrid.Grid(gridDiv, gridOptions)
+
+      const instance = new Grid({
+        scrollX: true,
+        scrollY: true,
+        el: document.getElementById('grid'),
+        columns: columnDefsFiltered,
+        data,
+      })
     })
     .catch((err) => {
       console.log(err)
