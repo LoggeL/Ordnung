784524fd58d549a19dd6b938cb00a6d0f7f1135d
Logge <hyper.xjo@gmail.com>
2023-09-28 18:27:01 +0200

    Add simple edit and sort functionality

--- frontend/src/routes/grid/[category]/+page.svelte
+++ frontend/src/routes/grid/[category]/+page.svelte
@@ -1,58 +1,72 @@
 <script>
-  import AgGridSvelte from 'ag-grid-svelte'
-  import 'ag-grid-community/styles/ag-grid.css'
-  import 'ag-grid-community/styles/ag-theme-alpine.css'
+  import AgGridSvelte from "ag-grid-svelte";
+  import "ag-grid-community/styles/ag-grid.css";
+  import "ag-grid-community/styles/ag-theme-alpine.css";
 
   // Connect to PocketBase Backend
-  import { pbStore } from 'svelte-pocketbase'
-  import { onMount } from 'svelte'
-  import { page } from '$app/stores'
+  import { pbStore } from "svelte-pocketbase";
+  import { onMount } from "svelte";
+  import { page } from "$app/stores";
 
-  const blacklisted = ['collectionId', 'collectionName', 'expand']
+  const blacklisted = ["collectionId", "collectionName", "expand"];
 
-  const { category } = $page.params
+  const { category } = $page.params;
 
-  console.log('category', category)
+  console.log("category", category);
 
   /**
    * @type {{ field: string; }[]}
    */
-  let columnDefs = []
+  let columnDefs = [];
   /**
    * @type {{ field: string; }[]}
    */
-  let rowData = []
+  let rowData = [];
+  let loaded = false;
+
+  function onCellValueChanged(event) {
+    console.log(event);
+
+    return true
+  }
 
   onMount(async () => {
-    $pbStore.autoCancellation(false)
-    await $pbStore.admins.authWithPassword('logge@duck.com', '404noswagfound')
+    $pbStore.autoCancellation(false);
+    await $pbStore.admins.authWithPassword("logge@duck.com", "404noswagfound");
 
     $pbStore
       .collection(category)
       .getFullList()
       .then((data) => {
-        console.log(data)
+        console.log(data);
         // Get column names
         columnDefs = Object.keys(data[0])
           .map((e) => {
-            if (blacklisted.includes(e)) return null
-            return { field: e }
+            if (blacklisted.includes(e)) return null;
+            return {
+              field: e,
+              filter: "agTextColumnFilter",
+              sortable: true,
+              editable: true,
+            };
           })
-          .filter((e) => e !== null)
+          .filter((e) => e !== null);
 
         // If it has name, put it first
         columnDefs.sort((a, b) => {
-          if (a.field === 'name') return -1
-          if (b.field === 'name') return 1
-          return 0
-        })
+          if (a.field === "name") return -1;
+          if (b.field === "name") return 1;
+          return 0;
+        });
 
         // Get row data
         rowData = data.map((e) => {
-          return { ...e }
-        })
-      })
-  })
+          return { ...e };
+        });
+
+        loaded = true;
+      });
+  });
 </script>
 
 <svelte:head>
@@ -61,9 +75,13 @@
 </svelte:head>
 
 <section>
-  <div class="ag-theme-alpine-dark">
-    <AgGridSvelte {columnDefs} {rowData} />
-  </div>
+  {#if loaded}
+    <div class="ag-theme-alpine-dark">
+      <AgGridSvelte {columnDefs} {rowData} {onCellValueChanged} />
+    </div>
+  {:else}
+    <h1>Loading...</h1>
+  {/if}
 </section>
 
 <style>
